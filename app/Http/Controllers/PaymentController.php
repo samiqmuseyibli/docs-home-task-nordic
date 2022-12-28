9e8b3816e62c5e8e0b96d7fd133408b34e1d8981
Samig Museyibli <samiqmuseyibli@gmail.com>
2022-12-28 17:24:44 +0400

    completed api side

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\PaymentStoreRequest;
 use App\Models\Loan;
 use App\Models\Payment;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
+use App\Models\PaymentOrder;
 
 class PaymentController extends Controller
 {
@@ -15,33 +13,36 @@ class PaymentController extends Controller
     {
 
         $loan = Loan::query()->where(['reference' => $request->description])->firstOrFail();
-//        dd($loan, $request->amount);
-        if((float)$request->amount == $loan->amount_to_pay){
+        $payment = Payment::create([
+            'firstname' => $request->post('firstname'),
+            'lastname' => $request->post('lastname'),
+            'amount' => $request->post('amount'),
+            'description' => $request->post('description'),
+            'ref_id' => $request->post('refId'),
+            'payment_date' => $request->post('paymentDate'),
+        ]);
+
+        if ((float)$request->post('amount') == $loan->amount_to_pay) {
 //            Mark loan as paid
 //            Mark payment as assigned
-        }else if((float)$request->amount > $loan->amount_to_pay){
+        } else if ((float)$request->amount > $loan->amount_to_pay) {
+            PaymentOrder::create([
+                'payment_id' => $payment->id,
+                'refund_amount' => (float)$request->post('amount') - $loan->amount_to_pay,
+                'status' => false
+            ]);
+
 //            Mark loan as paid
 //            Mark payment as partially assigned
 //            Create refund payment as separate entity called "Payment Order" with all necessary information
-        }else{
+        } else {
+//            $loan->amount_to_pay -= $request->amount;
 //            Mark payment as assigned
         }
 
 //        Payment received: communication sent to email and|or phone if defined
 //        Failed payments report: support@example.com
-        if(1){
-//            Loan fully paid: communication sent to email and|or phone if defined
-        }
-
-
-//        Payment::create([
-//            'firstname' => $request->firstname,
-//            'lastname' => $request->lastname,
-//            'amount' => $request->amount,
-//            'description' => $request->description,
-//            'ref_id' => $request->refId,
-//            'payment_date' => $request->paymentDate,
-//        ]);
-        return response()->json(['data' => $request->all()]);
+//        Loan fully paid: communication sent to email and|or phone if defined
+        return response()->json(['data' => 'Operation was successful!']);
     }
 }
